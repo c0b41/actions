8ff95ed01fadf0c6a521ba7ffe93278ea3762759
ayhankuru <ayhankuru@yandex.com.tr>
2019-02-15 03:28:46 +0300

    Updated

--- ampvalidator/entrypoint.sh
+++ ampvalidator/entrypoint.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-if [[ $@ -eq 0 ]] ; then
-    echo 'You must be set some args'
-    exit 0
-fi
-
-
 for url in "$@"
 do
     amphtml-validator "$url"
--- lighthouse/entrypoint.sh
+++ lighthouse/entrypoint.sh
@@ -2,9 +2,4 @@
 
 set -e
 
-if [[ $# -eq 0 ]] ; then
-    echo 'You must be set some args'
-    exit 0
-fi
-
 sh -c "lighthouse-ci $*"
